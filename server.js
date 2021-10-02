06db9918afbb90a956b957f5df0755a0954caafe
tanmay-kali <52064142+tanmay-kali@users.noreply.github.com>
2021-10-01 19:51:40 -0500

    initial

---
+++ server.js
@@ -0,0 +1,17 @@
+//creating a server
+const express = require("express");
+//calling express function
+const app = express();
+const ejs = require("ejs");
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.render("home"));
+
+// set template engine
+app.use(expressLayout);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));

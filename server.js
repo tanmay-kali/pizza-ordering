52f415c672a8dd445b43626226f2034b6351ec4b
tanmay-kali <52064142+tanmay-kali@users.noreply.github.com>
2021-10-09 12:58:38 -0500

    cart page and auth

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const express = require("express");
 //calling express function
 const app = express();
 const ejs = require("ejs");
+const favicon = require("serve-favicon");
 const path = require("path");
 const expressLayout = require("express-ejs-layouts");
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.render("home"));
-
 // set template engine
+
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+
+app.get("/", (req, res) => res.render("home"));
+app.get("/cart", (req, res) => res.render("customers/cart"));
+app.get("/login", (req, res) => res.render("auth/login"));
+app.get("/register", (req, res) => res.render("auth/register"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
